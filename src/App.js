489e2ca247261854b208c8f4c89a51f16d4281c8
Jeferson <jeferson.alves@dce.ufpb.br>
2020-06-25 17:11:16 -0300

    New Changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 import './App.css';
 import MenuITGirls from './components/menu';
+import ConteudoHome from './components/home';
  
 function App() {
   return (
     <div className="App">
+      
       <MenuITGirls/>
     </div>
   );
